6106f57d87e7e8acee5c6ebc272530d6115eff3f
Llorenç Seguí Capllonch <llorencseguicapllonch@MBP-de-Llorenc.home>
2020-04-07 19:27:17 +0200

    Common theme added

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import { ThemeProvider } from "@material-ui/styles";
+import {
+  CssBaseline,
+  createMuiTheme
+} from "@material-ui/core";
 import ServerMessage from './components/ServerMessage';
 import UsersList from './components/UsersList';
+import ApplicationBar from './components/ApplicationBar/ApplicationBar';
+
+const theme = createMuiTheme({
+  palette: {
+    type: "dark"
+  }
+});
 
 function App() {
 
   return (<>
-    <ServerMessage />
-    <UsersList />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <ApplicationBar />
+      <ServerMessage />
+      <UsersList />
+    </ThemeProvider>
   </>);
 }
 
