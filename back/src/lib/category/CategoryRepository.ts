9f4781014ecafceca8e478468aab46ea3c18a949
Llorenç Seguí Capllonch <llorencseguicapllonch@MBP-de-Llorenc.home>
2020-04-08 18:50:05 +0200

    Add id response categories request

--- back/src/lib/category/CategoryRepository.ts
+++ back/src/lib/category/CategoryRepository.ts
@@ -1,13 +1,14 @@
 import pool from './PgConnectionPool';
 
 export type Category = {
+    id: number,
     name: string,
     image: string
 }
 
 export class CategoryRepository {
     async get() {
-        const res = await pool.query<Category>('SELECT name, image FROM categories');
+        const res = await pool.query<Category>('SELECT id, name, image FROM categories');
 
         return res.rows;
     }
