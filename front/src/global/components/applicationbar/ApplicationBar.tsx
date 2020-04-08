4639cbbabb817c1f581a52aba061fc285ecd7fac
juanrechach <juanrechach@gmail.com>
2020-04-08 18:49:30 +0200

    Add menu, logo and ShoppingCart icon

--- front/src/global/components/applicationbar/ApplicationBar.tsx
+++ front/src/global/components/applicationbar/ApplicationBar.tsx
@@ -4,8 +4,11 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import MenuIcon from '@material-ui/icons/Menu';
+import { IconButton } from '@material-ui/core';
 import { AppContext } from "../../context/AppContext";
+import * as Logo from "./logo.png";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -18,8 +21,17 @@ const useStyles = makeStyles((theme: Theme) =>
     title: {
       flexGrow: 1,
     },
+    logo: {
+      width: "160px",
+      paddingTop: "8px"
+    }
   }),
 );
+
+const goToCardList = () => {
+  // TODO when we have the router and the Card List screen
+  console.log("goToCardList");
+}
 
 export const ApplicationBar = () => {
   const classes = useStyles();
@@ -29,10 +41,16 @@ export const ApplicationBar = () => {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
           <Typography variant="h6" className={classes.title}>
-            Els illencs
+            < img className={classes.logo} src={Logo.default} alt="" />
+
           </Typography>
-          <Button color="inherit">Cistella</Button>
+          <IconButton onClick={goToCardList} edge="start" color="inherit" aria-label="menu">
+            <ShoppingCartIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
     </div>
