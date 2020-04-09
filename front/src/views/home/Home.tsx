de875c5f8e5b88090c3f73933e5b9d270db01d53
Xavier Coll <xavicollbagur@gmail.com>
2020-04-09 09:45:15 +0200

    Front: test change

--- front/src/views/home/Home.tsx
+++ front/src/views/home/Home.tsx
@@ -2,7 +2,7 @@ import React, { FC } from "react";
 
 const Home: FC = () => (<>
     <h1>Pàgina d'inici</h1>
-    <p>Accedeix al menú (botó superior esquerra) per a accedir a una altra secció</p>
+    <p>Accedeix al menú (botó superior esquerra) per a accedir a una altra secció.</p>
 </>);
 
 export default Home;
