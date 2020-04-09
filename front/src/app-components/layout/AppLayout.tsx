8f992ecaebe23a3f8238e1f1a70a6395f269e77f
juanrechach <juanrechach@gmail.com>
2020-04-09 17:37:14 +0200

    Add and remove products to the shopping cart

--- front/src/app-components/layout/AppLayout.tsx
+++ front/src/app-components/layout/AppLayout.tsx
@@ -1,7 +1,10 @@
-import React, { FC, ReactNode, useState } from "react";
+import React, { FC, ReactNode, useState, useEffect } from "react";
 import { ApplicationBar } from "./ApplicationBar";
 import { Switch, Route, Link } from "react-router-dom";
 import { makeStyles, Drawer, List, ListItem, ListItemText } from "@material-ui/core";
+import { AppContext } from "../context/AppContext";
+import { ShoppingCartApiClient } from "../../api/ShoppingCartApiClient";
+import { Product } from "../../models/product/Product";
 
 type Page = {
   label: string,
@@ -15,39 +18,70 @@ type AppLayoutProps = {
 
 const useStyles = makeStyles((theme) => ({
   content: {
-    padding: 20
+    padding: 20,
+    marginTop: 30
   }
 }))
 
-const AppLayout: FC<AppLayoutProps> = (props) => {
-  const classes = useStyles(props);
-  const [drawerOpen, setDrawerOpen] = useState(false);
+const shoppingCartApiClient = new ShoppingCartApiClient();
 
-  const openDrawer = () =>  setDrawerOpen(true);
+const AppLayout: FC<AppLayoutProps> = (props) => {
+  const styles = useStyles(props);
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [shoppingCartProducts, setShoppingCartProducts] = useState<Product[]>([]);
+
+  const openDrawer = () => setDrawerOpen(true);
   const closeDrawer = () => setDrawerOpen(false);
 
-  return(<>
+  useEffect(() => {
+    const getShoppingCartProducts = async () => setShoppingCartProducts(await shoppingCartApiClient.getItems());
+    getShoppingCartProducts();
+  }, []);
+
+
+  return (<AppContext.Provider value={{
+    shoppingCart: {
+      products: shoppingCartProducts,
+      addProduct: (product: Product) => {
+        const nextShoppingCartProducts = shoppingCartProducts.slice();
+        nextShoppingCartProducts.push(product);
+        shoppingCartApiClient.saveItems(nextShoppingCartProducts);
+        setShoppingCartProducts(nextShoppingCartProducts);
+      },
+      deleteProduct: (productId: number) => {
+        const nextShoppingCartProducts = shoppingCartProducts.slice();
+        const index = nextShoppingCartProducts.findIndex(({ id }) => id === productId);
+        console.log(index)
+        if (index === -1) {
+          return;
+        }
+        nextShoppingCartProducts.splice(index, 1);
+        shoppingCartApiClient.saveItems(nextShoppingCartProducts);
+        setShoppingCartProducts(nextShoppingCartProducts);
+      },
+    }
+  }}>
     <ApplicationBar onMenuButtonClick={openDrawer} />
-    <main className={classes.content}>
+    <main className={styles.content}>
       <Drawer anchor="left" open={drawerOpen} onClose={closeDrawer}>
         <List>
           {props.pages.map((p) => (
             <ListItem button component={Link} to={p.path} onClick={closeDrawer} key={p.path}>
-              <ListItemText primary={p.label}/>
+              <ListItemText primary={p.label} />
             </ListItem>
           ))}
         </List>
       </Drawer>
       <Switch>
-        {props.pages.map((p, i) => 
-            <Route path={p.path} key={i} exact={p.path === '/'}>
-              {p.content}
-            </Route>
+        {props.pages.map((p, i) =>
+          <Route path={p.path} key={i} exact={p.path === '/'}>
+            {p.content}
+          </Route>
         )}
       </Switch>
     </main>
 
-  </>);
+  </AppContext.Provider>);
 };
 
 export default AppLayout;
--- front/src/pages/shoppingCart/ProductShoppingCartItem.tsx
+++ front/src/pages/shoppingCart/ProductShoppingCartItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import CardContent from '@material-ui/core/CardContent';
@@ -7,6 +7,7 @@ import Button from '@material-ui/core/Button';
 import { Product } from '../../models/product/Product';
 import { ProductInfoItem } from '../../components/ProductInfoItem';
 import { ShoppingCartApiClient } from '../../api/ShoppingCartApiClient';
+import { AppContext } from '../../app-components';
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -49,12 +50,10 @@ export interface ProductShoppingCartItemProps {
 };
 
 export const ProductShoppingCartItem: React.FunctionComponent<ProductShoppingCartItemProps> = ({ product }) => {
+  const { shoppingCart } = useContext(AppContext);
   const classes = useStyles();
 
-  const deleteProduct = async () => {
-    await shoppingCartApiClient.deleteItem(product.id);
-    // TODO shopping cart number should be updated
-  }
+  const deleteProduct = () => shoppingCart.deleteProduct(product.id);
 
   return (
     <ProductInfoItem product={product} >
@@ -65,7 +64,7 @@ export const ProductShoppingCartItem: React.FunctionComponent<ProductShoppingCar
               <TextField className={classes.quantity} id="outlined-basic" type="number" size="small" label="Quantitat" variant="outlined" />
             </Grid>
             <Grid item xs={6} md={3}>
-              <Button onClick={deleteProduct}variant="contained" size="medium" color="primary">
+              <Button onClick={deleteProduct} variant="contained" size="medium" color="primary">
                 Elimnar
               </Button>
             </Grid>
