64920b42c020ef18a5c62c236cbe4720e3c331df
Xavier Coll <xavicollbagur@gmail.com>
2020-04-09 11:25:43 +0200

    front: fix

--- front/nginx/entrypoint.sh
+++ front/nginx/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-envsubst '$PORT' < /container-data/nginx.template.conf > /etc/nginx/nginx.conf
+envsubst '$PORT' < /nginx-container-data/nginx.template.conf > /etc/nginx/nginx.conf
 
 nginx -g "daemon off;"
