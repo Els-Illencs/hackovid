d38963ec85ec1e04f97376c6e03d0734b8a0ec65
juanrechach <juanrechach@gmail.com>
2020-04-09 13:11:53 +0200

    Add shopping cart header

--- front/src/pages/shoppingCart/ShoppingCart.tsx
+++ front/src/pages/shoppingCart/ShoppingCart.tsx
@@ -1,22 +1,60 @@
 import React, { useState, useEffect } from "react";
 import { Product } from "../../models/product/Product";
 import { ShoppingCartApiClient } from "../../api/ShoppingCartApiClient";
+import { Card, makeStyles, Theme, createStyles, CardHeader, CardActionArea, Button, ButtonGroup, CardContent } from "@material-ui/core";
 
 const apiClient = new ShoppingCartApiClient();
 
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    header: {
+      marginTop: 30,
+      paddingLeft: 16,
+      paddingRight: 16,
+      paddingTop: 0,
+      paddingBottom: 16
+    },
+    button: {
+      width: "100%"
+    }
+  }),
+);
+
 export const ShoppingCart: React.FunctionComponent = () => {
-    const [products, setProducts] = useState([] as Product[]);
+  const [products, setProducts] = useState([] as Product[]);
 
-    useEffect(() => {
-        const getProducts = async () => {
-            setProducts(await apiClient.getItems());
+  const classes = useStyles();
+
+  useEffect(() => {
+    const getProducts = async () => {
+      setProducts(await apiClient.getItems());
+    }
+    getProducts();
+  }, []);
+
+  const totalPrice = products.reduce((priceSum, { price }) => priceSum + price, 0);
+
+  return (
+    <div>
+      <Card className={classes.header}>
+        {products.length !== 0 ? // TODO !== condition is temporal, change for ===
+          <CardContent>
+            Cistella buida
+         </CardContent> :
+          <>
+            <h4>Resum de la cistella</h4>
+            <CardContent>
+              Subtotal ({products.length}) productes: {totalPrice} €
+            </CardContent>
+            <CardActionArea>
+              <Button className={classes.button} variant="contained" size="large" color="secondary">
+                Tramitar comanda
+              </Button>
+            </CardActionArea>
+          </>
         }
-        getProducts();
-    }, []);
-
-    return (
-        <>
-            Shopping Cart page
-        </>
-    );
+      </Card>
+    </div>
+  );
 }
