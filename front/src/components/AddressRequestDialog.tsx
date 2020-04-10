4706f82077cf797b8f1774e929dd4f5ba232a13c
Llorenç Seguí Capllonch <llorencseguicapllonch@MBP-de-Llorenc.home>
2020-04-10 16:54:39 +0200

    Improve styles gmaps

--- front/src/components/AddressRequestDialog.tsx
+++ front/src/components/AddressRequestDialog.tsx
@@ -17,6 +17,7 @@ const styles = (theme: Theme) =>
     root: {
       margin: 0,
       padding: theme.spacing(2),
+      width: '100%'
     },
     suggestionAutoComplete: {
       cursor: 'pointer',
