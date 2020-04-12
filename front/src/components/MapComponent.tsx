99b0dc8b68762a473e591ed7acf6708172a2d1e7
Llorenç Seguí Capllonch <llorencseguicapllonch@MBP-de-Llorenc.home>
2020-04-12 16:36:03 +0200

    Fix bug mapa

--- front/src/components/MapComponent.tsx
+++ front/src/components/MapComponent.tsx
@@ -15,7 +15,7 @@ let travelModeList: any;
 
 export const MapComponent = compose(
   withProps({
-    //googleMapURL: "https://maps.googleapis.com/maps/api/js?key=" + apiKey + "&v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=" + apiKey + "&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
